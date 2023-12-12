e04e3298fd43cc6f3ba90d9b4180488931dd4db6
Jonh Nino <jonhninoa@gmail.com>
2023-12-11 21:32:02 -0500

    11/12/23 Lista de modelos de OPENAI

--- mi.py
+++ mi.py
@@ -1,6 +1,11 @@
-import os 
+import os
+import openai
+
 from dotenv import load_dotenv
 
 load_dotenv()
-
-api_key=os.getenv('OPENAI_API_KEY')
+api_key = os.getenv('OPENAI_API_KEY')
+print(f"API Key: {api_key}")  # Esto imprimirá tu clave de API, asegúrate de que es correcta
+openai.api_key = api_key
+modelos = openai.Model.list()
+print(modelos)
